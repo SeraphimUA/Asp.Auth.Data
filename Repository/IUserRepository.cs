96e478d6a23555b7602d72687ba614bf2b484810
SeraphimUA <mikultvua@gmail.com>
2021-10-29 18:51:39 +0300

    Added project

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Data.Repository
+{
+    public interface IUserRepository
+    {
+        User Get(string username, string password);
+        void Add(User user);
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Auth.Data.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AuthDbContext _context;
+
+        public UserRepository(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(User user)
+        {
+            _context.Users.Add(user);
+        }
+
+        public User Get(string username, string password)
+        {
+            return _context.Users
+                .Include(u => u.Roles)
+                .ThenInclude(r => r.Permissions)
+                .FirstOrDefault(u =>
+                    u.Username.Equals(username) &&
+                    u.HashedPassword.Equals(password));
+        }
+    }
+}
